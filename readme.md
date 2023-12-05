b1708b102e8fd5546bc4831b50c14f290c472354
HoiGe <rd_idk5682@outlook.com>
2023-12-05 13:35:43 +0800

    init

--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-lastqa?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-lastqa)
 
-重自 RscBot QA插件
+重写自 RscBot QA插件
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,35 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema } from "koishi";
 
-export const name = 'lastqa'
+export const name = "lastqa";
 
 export interface Config {}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({});
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx
+    .command("choice <o1> <o2>")
+    .shortcut(/!(.+)还是(.+)/, { args: ["$1", "$2"] })
+    .shortcut(/！(.+)还是(.+)/, { args: ["$1", "$2"] })
+    .action(async ({ session, options }, o1, o2) => {
+      let i1 = o1.replace(/我/g, "你");
+      let i2 = o2.replace(/我/g, "你");
+      session.send("当然是" + random(i1, i2) + "咯~");
+    });
+
+  ctx
+    .command("decide <o1> <o2>")
+    .shortcut(/!(.+)不(.+)/, { args: ["$1", "$2"] })
+    .shortcut(/！(.+)不(.+)/, { args: ["$1", "$2"] })
+    .action(async ({ session, options }, o1, o2) => {
+      if (o1 == o2[0]) {
+        let i2 = o2.replace(/我/g, "你");
+        session.send(random(i2, "不" + i2));
+      }
+    });
+}
+
+function random(o1: string, o2: string): string {
+  const randomValue = Math.random();
+  return randomValue < 0.5 ? o1 : o2;
 }
